0e3a5a3d18d0f9355617d69d2eee4cbabb79cc5f
mauricio <mauriciopfleger7@gmail.com>
2024-06-13 18:50:46 -0300

    api de clubes

--- Controllers/TesteController.cs
+++ Controllers/TesteController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Net;
+using MySql.Data.MySqlClient;
 
 namespace futebol.Controllers
 {
@@ -13,6 +15,35 @@ namespace futebol.Controllers
         public IActionResult ConsultarTutor() // Método que irá realizar o que queremos
         {
             return Ok($@"Olá Mundo!!!"); // Retorna alguma informação com sucesso, lembrando que Ok é sucesso.
+        }
+
+        [HttpGet("clube")]
+        [ProducesResponseType(typeof(Ok), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BadRequest), (int)HttpStatusCode.BadRequest)]
+        public IActionResult ConsultaClube() 
+        {
+            string connectionString = "Server=localhost;Port=3306;Database=sys;Uid=root;Pwd=admin;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            string query = "SELECT nome FROM sys.clubes WHERE id = 2";
+
+            MySqlCommand command = new MySqlCommand(query, connection);
+            
+            connection.Open();
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                string clube = reader.GetString("nome");
+                connection.Close();
+                return Ok(clube);
+            }
+            else {
+                connection.Close();
+                return BadRequest("Erro");
+            }
         }
     }
 }
